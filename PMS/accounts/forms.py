9398f8d0d575dd01d9fb62a5cbb5bd3a7290c494
Greg Emmanuel Alenton <gremmanuelenton02@gmail.com>
2015-11-10 15:50:52 +0800

    tried changing database again

--- PMS/accounts/forms.py
+++ PMS/accounts/forms.py
@@ -1,3 +1,22 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
+
+class StudentForm(UserCreationForm):
+    username = forms.CharField(
+        required=True
+    )
+
+    password1 = forms.CharField(
+        required=True,
+        label=("Password")
+    )
+
+    first_name = forms.CharField(
+        required=True
+    )
+
+    last_name = forms.CharField(
+        required=True
+    )
 
