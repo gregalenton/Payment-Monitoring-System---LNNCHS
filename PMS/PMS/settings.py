26d6570ea70ad44d6e41ae994b8d34d13d7d5d87
Mark Robin <markrobin815@gmail.com>
2015-11-10 14:23:33 +0800

    fixed errors in migration

--- PMS/PMS/settings.py
+++ PMS/PMS/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Database',
+    'PMS',
     'accounts',
-    'database',
 )
 
 MIDDLEWARE_CLASSES = (
