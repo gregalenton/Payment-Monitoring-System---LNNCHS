b6a756b016f1aaffccd6e32ab6693f77f44bef15
Greg Emmanuel Alenton <gremmanuelenton02@gmail.com>
2015-11-18 17:28:03 +0800

    removed database in settings

--- PMS/PMS/settings.py
+++ PMS/PMS/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'funds',
     'accounts',
+    'funds',
 )
 
 MIDDLEWARE_CLASSES = (
