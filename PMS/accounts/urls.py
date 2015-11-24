9a62c55c4ed9d40bdbab962177ba949df06da092
Mark Robin <markrobin815@gmail.com>
2015-11-24 15:06:45 +0800

    enable logout of student users

--- PMS/accounts/urls.py
+++ PMS/accounts/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 urlpatterns = patterns('',
         url(r'^index/$', views.index, name='index'),
         url(r'^accounts/login/$', views.user_login, name='login'),
-        url(r'^accounts/home/$', views.admin_home, name='home'),
+        url(r'^accounts/home/$', views.admin_home, name='adminhome'),
         url(r'^accounts/logout/$', views.user_logout, name='logout'),
         url(r'^accounts/addstudent/$',views.add_student, name='addstudent'),
         ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
